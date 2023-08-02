69a48d2dc3ce465a1e24d4a827085d8d06abffb9
buddy <litoubuddy@gmail.com>
2023-08-02 17:35:35 +0800

    feature: add postcss-plugin-px2rem

--- craco.config.js
+++ craco.config.js
@@ -2,36 +2,35 @@
 const isAnalyze = process.env.ANALYZE
 const path = require('path')
 const webpackBundleAnalyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-const webpack = require("webpack")
+const webpack = require('webpack')
+
+const addPath = (dir) => path.resolve(__dirname, dir)
 
 module.exports = {
-    // 自定义 fork 的react-scripts 路径
-    // reactScriptsVersion:''
-    webpack:{
+	// 自定义 fork 的react-scripts 路径
+	// reactScriptsVersion:''
+	webpack: {
+		configure: (webpackConfig, { env, paths }) => {
+			// 打包 moment 指定语言，一般moment 也是用dayjs 来替代的
+			webpackConfig.plugins.push(new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /zh-cn/))
+			// analyze
+			if (isAnalyze) webpackConfig.plugins.push(new webpackBundleAnalyzer())
 
-			configure:(webpackConfig,{env,paths})=>{
-				// 打包 moment 指定语言，一般moment 也是用dayjs 来替代的
-				webpackConfig.plugins.push(new webpack.ContextReplacementPlugin(/moment[/\\]locale$/,/zh-cn/))
-				// analyze
-				if(isAnalyze)
-					webpackConfig.plugins.push(new webpackBundleAnalyzer());
-				
-				return webpackConfig
-			},
+			return webpackConfig
+		},
 
-			externals:{
-				// cdn 资源不打包
-			},
-			
-			alias:{
-					"@components": path.resolve(__dirname,'src/components') ,
-					"@resources":'./src/resources',
-					"@assets":'./src/assets',
-					"@pages":'./src/pages',
-					'@store':'./src/store',
-					"@constants":"./src/constants",
-					"@utility":'./src/utility',
-					"@hooks":'./src/hooks'
-			}
-    }
+		externals: {
+			// cdn 资源不打包
+		},
+		alias: {
+			'@components': addPath('src/components'),
+			'@resources': addPath('./src/resources'),
+			'@assets': addPath('./src/assets'),
+			'@pages': addPath('./src/pages'),
+			'@store': addPath('./src/store'),
+			'@constants': addPath('./src/constants'),
+			'@utility': addPath('./src/utility'),
+			'@hooks': addPath('./src/hooks')
+		}
+	}
 }
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,17 @@
-module.exports={
-	"plugins": {
-		"autoprefixer": {}
+module.exports = {
+	plugins: {
+		autoprefixer: {},
+		'postcss-plugin-px2rem': {
+			rootValue: 75,
+			unitPrecision: 2,
+			propWhiteList: [],
+			propBlackList: ['border', 'text-shadow', 'box-shadow'],
+			exclude: /node_modules/i,
+			selectorBlackList: [],
+			ignoreIdentifier: false,
+			replace: true,
+			mediaQuery: false,
+			minPixelValue: 0
+		}
 	}
 }
