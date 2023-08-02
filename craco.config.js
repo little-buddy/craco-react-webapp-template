b653b7810941924b5e0fdef6b5980c9375437eba
buddy <litoubuddy@gmail.com>
2023-08-02 17:12:38 +0800

    feature: add webpack-bundle-analyze

--- craco.config.js
+++ craco.config.js
@@ -1,19 +1,37 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const isAnalyze = process.env.ANALYZE
 const path = require('path')
+const webpackBundleAnalyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const webpack = require("webpack")
 
 module.exports = {
     // 自定义 fork 的react-scripts 路径
     // reactScriptsVersion:''
     webpack:{
-        alias:{
-            "@components": path.resolve(__dirname,'src/components') ,
-            "@resources":'./src/resources',
-            "@assets":'./src/assets',
-            "@pages":'./src/pages',
-            '@store':'./src/store',
-            "@constants":"./src/constants",
-            "@utility":'./src/utility',
-            "@hooks":'./src/hooks'
-        }
+
+			configure:(webpackConfig,{env,paths})=>{
+				// 打包 moment 指定语言，一般moment 也是用dayjs 来替代的
+				webpackConfig.plugins.push(new webpack.ContextReplacementPlugin(/moment[/\\]locale$/,/zh-cn/))
+				// analyze
+				if(isAnalyze)
+					webpackConfig.plugins.push(new webpackBundleAnalyzer());
+				
+				return webpackConfig
+			},
+
+			externals:{
+				// cdn 资源不打包
+			},
+			
+			alias:{
+					"@components": path.resolve(__dirname,'src/components') ,
+					"@resources":'./src/resources',
+					"@assets":'./src/assets',
+					"@pages":'./src/pages',
+					'@store':'./src/store',
+					"@constants":"./src/constants",
+					"@utility":'./src/utility',
+					"@hooks":'./src/hooks'
+			}
     }
 }
