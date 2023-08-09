9642cfc3e946663567c0e2fc5ff9c5b0badc29e9
buddy <litoubuddy@gmail.com>
2023-08-09 17:17:14 +0800

    refactor: carco.config.js change to fn-mode avoid sth fn run twice

--- craco.config.js
+++ craco.config.js
@@ -12,40 +12,48 @@ const addPath = dir => path.resolve(__dirname, dir);
 	carco 的 babel 只能在 carco.config.js 里面配置
  */
 
-module.exports = {
-	// 自定义 fork 的react-scripts 路径
-	// reactScriptsVersion:''
-	webpack: {
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		configure: (webpackConfig, { env, paths }) => {
-			// 打包 moment 指定语言，一般moment 也是用dayjs 来替代的
-			webpackConfig.plugins.push(
-				new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /zh-cn/)
-			);
-			// analyze
-			if (isAnalyze) webpackConfig.plugins.push(new WebpackBundleAnalyzer());
+/* 
+	carco.config.js 不保证只加载一次
+	see https://github.com/dilanx/craco/issues/518
+ */
+module.exports = () => {
+	console.log('run get craco-config-object');
 
-			webpackConfig.plugins.push(
-				new StyleLintPlugin()
-				// 默认处理['css', 'scss', 'sass']
-			);
+	return {
+		// 自定义 fork 的react-scripts 路径
+		// reactScriptsVersion:''
+		webpack: {
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			configure: (webpackConfig, { env, paths }) => {
+				// 打包 moment 指定语言，一般moment 也是用dayjs 来替代的
+				webpackConfig.plugins.push(
+					new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /zh-cn/)
+				);
+				// analyze
+				if (isAnalyze) webpackConfig.plugins.push(new WebpackBundleAnalyzer());
 
-			return webpackConfig;
-		},
-		babel: {},
-		style: {
-			postcss: {
+				webpackConfig.plugins.push(
+					new StyleLintPlugin()
+					// 默认处理['css', 'scss', 'sass']
+				);
+
+				return webpackConfig;
+			},
+			babel: {},
+			style: {
+				postcss: {
+					mode: 'file',
+				},
+			},
+			eslint: {
 				mode: 'file',
 			},
+			externals: {
+				// cdn 资源不打包
+			},
+			alias: {
+				'@': addPath('./src'),
+			},
 		},
-		eslint: {
-			mode: 'file',
-		},
-		externals: {
-			// cdn 资源不打包
-		},
-		alias: {
-			'@': addPath('./src'),
-		},
-	},
+	};
 };
