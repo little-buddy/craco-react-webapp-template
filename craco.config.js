249563b79a9e877ac9a99c3a3b926000e2bd3e74
buddy <litoubuddy@gmail.com>
2023-08-14 22:43:00 +0800

    chore: Extract the plug-in logic to cracoPlugin.js

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const isAnalyze = process.env.ANALYZE;
-const path = require('path');
-const { when, removePlugins } = require('@craco/craco');
+const { when } = require('@craco/craco');
 
 const webpack = require('webpack');
 const WebpackBundleAnalyzer =
@@ -9,12 +7,16 @@ const WebpackBundleAnalyzer =
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const WepbackBar = require('webpackbar');
 const Smp = require('speed-measure-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const PreloadWebapckPlugin = require('@vue/preload-webpack-plugin');
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
-const isEnvProduction = process.env.NODE_ENV === 'production';
-const addPath = dir => path.resolve(__dirname, dir);
+const {
+	isEnvProduction,
+	addPath,
+	splitChunks,
+	getBabelConf,
+	refactorEntry,
+	isAnalyze,
+} = require('./cracoplugin');
 
 /* 
 	carco 的 babel 只能在 carco.config.js 里面配置
@@ -25,152 +27,30 @@ const addPath = dir => path.resolve(__dirname, dir);
 	see https://github.com/dilanx/craco/issues/518
  */
 module.exports = () => {
-	console.log(isEnvProduction, 'run get craco-config-object');
+	console.log(addPath('./src'));
 
 	return {
 		// 自定义 fork 的react-scripts 路径
 		// reactScriptsVersion:''
 		webpack: new Smp().wrap({
 			configure: (config, { env, paths }) => {
-				// eslint-disable-next-line no-param-reassign
-				config.entry = {
-					app: config.entry,
-				};
-				// eslint-disable-next-line no-param-reassign
-				config.optimization.splitChunks = {
-					cacheGroups: {
-						reactLib: {
-							test: /[\\/]node_modules[\\/](react|react-dom|react-router-dom|@reduxjs\/toolkit\/dist)[\\/]/,
-							name: 'react-lib',
-							chunks: 'all',
-							enforce: true,
-							priority: 40,
-							reuseExistingChunk: true,
-						},
-						vendors: {
-							name: `chunk-vendors`,
-							test: /[\\/]node_modules[\\/]/,
-							priority: -10,
-							chunks: 'initial',
-						},
-						common: {
-							name: `chunk-common`,
-							minChunks: 2,
-							priority: -20,
-							chunks: 'initial',
-							reuseExistingChunk: true,
-						},
-					},
-				};
-
-				removePlugins(config, (value, index, array) => {
-					const pluginName = Object.getPrototypeOf(array[index]).constructor
-						.name;
-
-					if (pluginName === 'HtmlWebpackPlugin' && index !== 0) {
-						return true;
-					}
-
-					if (['WebpackManifestPlugin'].indexOf(pluginName) > -1) {
-						const plugin = array[index];
-						if (!(plugin instanceof WebpackManifestPlugin)) {
-							return true;
-						}
-					}
-
-					return false;
-				});
+				refactorEntry(config);
+				splitChunks(config);
 
 				return config;
 			},
 			plugins: [
-				[
-					new HtmlWebpackPlugin({
-						inject: 'body',
-						template: addPath('./public/index.html'),
-						// external的js-cdn 可以配置在这里
-						cdn: {
-							js: [],
-							css: [],
-						},
-						...(isEnvProduction
-							? {
-									minify: {
-										removeComments: true,
-										collapseWhitespace: true,
-										removeRedundantAttributes: true,
-										useShortDoctype: true,
-										removeEmptyAttributes: true,
-										removeStyleLinkTypeAttributes: true,
-										keepClosingSlash: true,
-										minifyJS: true,
-										minifyCSS: true,
-										minifyURLs: true,
-									},
-							  }
-							: undefined),
-					}),
-					'prepend',
-				],
-
-				new WepbackBar(),
+				new WepbackBar({
+					profile: true,
+					color: '#fa8c16',
+				}),
+				// new HardSourceWebpackPlugin(),
 				new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /zh-cn/),
 				// 默认处理['css', 'scss', 'sass']
 				new StyleLintPlugin(),
 				...when(isAnalyze, () => [new WebpackBundleAnalyzer()], []),
-				// preload
-				// [
-				// 	new PreloadWebapckPlugin({
-				// 		rel: 'preload',
-				// 		include: 'initial',
-				// 		fileBlacklist: [/\.map$/, /hot-update\.js$/],
-				// 	}),
-				// 	'append',
-				// ],
-				// // prefetch
-				// [
-				// 	new PreloadWebapckPlugin({
-				// 		rel: 'prefetch',
-				// 		include: 'asyncChunks',
-				// 	}),
-				// 	'append',
-				// ],
-				new WebpackManifestPlugin({
-					fileName: 'asset-manifest.json',
-					// publicPath: paths.publicUrlOrPath,
-					generate: (seed, files, entrypoints) => {
-						const manifestFiles = files.reduce((manifest, file) => {
-							// eslint-disable-next-line no-param-reassign
-							manifest[file.name] = file.path;
-							return manifest;
-						}, seed);
-						const entrypointFiles = entrypoints.app.filter(
-							fileName => !fileName.endsWith('.map')
-						);
-
-						return {
-							files: manifestFiles,
-							entrypoints: entrypointFiles,
-						};
-					},
-				}),
 			],
-			babel: {
-				// react 默认配置了
-				// loaderOptions: {
-				// 	cacheDirectory: true,
-				// },
-				plugins: [
-					[
-						'import',
-						{
-							libraryName: 'lodash',
-							libraryDirectory: '',
-							camel2DashComponentName: false,
-						},
-					],
-				],
-			},
+			babel: getBabelConf(),
 			style: {
 				postcss: {
 					mode: 'file',
