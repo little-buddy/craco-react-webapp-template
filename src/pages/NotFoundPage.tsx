b1a12d8105541989bf044373529d38e274dfb41a
buddy <litoubuddy@gmail.com>
2023-09-22 21:17:17 +0800

    chore: update eslintrc.js

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,34 +1,62 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { ROUTES } from '../resources/routes-constants';
+import React, { PropsWithChildren, useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { ROUTES } from '../resources/routes-constants';
 
-const NotFoundPage: React.FC = () => {
-	const navigate = useNavigate();
+// const NotFoundPage: React.FC = () => {
+// 	const navigate = useNavigate();
 
-	/**
-	 * Call this function to redirect the user to the homepage.
-	 */
-	const redirectToHomePage = () => {
-		navigate(ROUTES.HOMEPAGE_ROUTE);
-	};
+// 	/**
+// 	 * Call this function to redirect the user to the homepage.
+// 	 */
+// 	const redirectToHomePage = () => {
+// 		navigate(ROUTES.HOMEPAGE_ROUTE);
+// 	};
+
+// 	return (
+// 		<div
+// 			style={{
+// 				position: 'relative',
+// 				width: '100%',
+// 				display: 'flex',
+// 				justifyContent: 'center',
+// 				alignItems: 'center',
+// 				flexDirection: 'column',
+// 			}}
+// 		>
+// 			<h1 style={{ fontSize: '4em' }}>Oops 404!</h1>
+// 			<span style={{ cursor: 'pointer' }} onClick={() => redirectToHomePage()}>
+// 				Homepage
+// 			</span>
+// 		</div>
+// 	);
+// };
+
+// children 作为 props 有自带 memo 的功能
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function AA() {
+	console.log('AA');
+	return 'AA';
+}
+
+const B = ({ children }: PropsWithChildren) => {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [v, setV] = useState(1);
+	console.log(children);
 
 	return (
 		<div
-			style={{
-				position: 'relative',
-				width: '100%',
-				display: 'flex',
-				justifyContent: 'center',
-				alignItems: 'center',
-				flexDirection: 'column',
+			onClick={() => {
+				setV(Math.random());
 			}}
 		>
-			<h1 style={{ fontSize: '4em' }}>Oops 404!</h1>
-			<span style={{ cursor: 'pointer' }} onClick={() => redirectToHomePage()}>
-				Homepage
-			</span>
+			{children}
 		</div>
 	);
 };
 
-export default NotFoundPage;
+const A: React.FC = () => (
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+	<B>阿斯顿发斯蒂芬</B>
+);
+export default A;
